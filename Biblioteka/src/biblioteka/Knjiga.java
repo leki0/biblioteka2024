f62672554ebd71027d6b8c218ab57b1e7cd47e07
leki0 <aleksastancic9@gmail.com>
2024-03-25 20:27:21 +0100

    Dodata klasa Autor

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,11 +1,13 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
 
 	private String naslov;
 	private long isbn;
-	private String autor;
+	private List<Autor> autori;
 	private String izdavac;
 	private int izdanje;
-	
+
 }
