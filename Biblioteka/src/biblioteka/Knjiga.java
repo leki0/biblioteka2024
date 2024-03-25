67463240304a07c116096900047d34b2c7bc4ccc
leki0 <aleksastancic9@gmail.com>
2024-03-25 21:16:28 +0100

    dodati geteri i seteri, kao i toString i Equals metode

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,6 +1,7 @@
 package biblioteka;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Knjiga {
 
@@ -9,5 +10,58 @@ public class Knjiga {
 	private List<Autor> autori;
 	private String izdavac;
 	private int izdanje;
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public List<Autor> getAutori() {
+		return autori;
+	}
+	public void setAutori(List<Autor> autori) {
+		this.autori = autori;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(autori, isbn, izdanje, izdavac, naslov);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		return Objects.equals(autori, other.autori) && isbn == other.isbn && izdanje == other.izdanje
+				&& Objects.equals(izdavac, other.izdavac) && Objects.equals(naslov, other.naslov);
+	}
+	
+	
 
 }
